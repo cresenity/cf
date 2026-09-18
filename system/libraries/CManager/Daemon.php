039f37b1f25ad7201c7a58a789e1e995a98ea05e
Hery <hery@ittron.co.id>
2026-09-18 09:43:46 +0700

    chore(CManager_Daemon): add docblocks, compare count() instead of the array in getServiceName()

--- system/libraries/CManager/Daemon.php
+++ system/libraries/CManager/Daemon.php
@@ -1,6 +1,9 @@
 <?php
 
 final class CManager_Daemon {
+    /**
+     * @var CManager_Daemon
+     */
     protected static $instance;
 
     protected $daemons = [];
@@ -22,6 +25,13 @@ final class CManager_Daemon {
         return self::$instance;
     }
 
+    /**
+     * Get the status of a daemon service.
+     *
+     * @param string $serviceClass
+     *
+     * @return array
+     */
     public function getDaemonStatus($serviceClass) {
         $ttl = 5; // detik
 
@@ -46,6 +56,13 @@ final class CManager_Daemon {
         return $status;
     }
 
+    /**
+     * Register a daemon service.
+     *
+     * @param string $class
+     * @param null|string $name
+     * @param null|string $group
+     */
     public function registerDaemon($class, $name = null, $group = null) {
         if ($name == null) {
             $name = carr::last(explode('_', $class));
@@ -88,48 +105,120 @@ final class CManager_Daemon {
         return count($this->getGroupsKey()) > 0;
     }
 
+    /**
+     * Get the status of a daemon service.
+     *
+     * @param string $className
+     *
+     * @return mixed
+     */
     public function status($className) {
         return CDaemon::createRunner($className)->status();
     }
 
+    /**
+     * Start a daemon service.
+     *
+     * @param string $className
+     *
+     * @return mixed
+     */
     public function start($className) {
         return CDaemon::createRunner($className)->run();
     }
 
+    /**
+     * Stop a daemon service.
+     *
+     * @param string $className
+     * @param bool $force
+     *
+     * @return mixed
+     */
     public function stop($className, $force = false) {
         return CDaemon::createRunner($className)->stop($force);
     }
 
+    /**
+     * Check if a daemon service is running.
+     *
+     * @param string $className
+     *
+     * @return bool
+     */
     public function isRunning($className) {
         return CDaemon::createRunner($className)->isRunning();
     }
 
+    /**
+     * Rotate the log file for a daemon service.
+     *
+     * @param string $className
+     *
+     * @return mixed
+     */
     public function rotateLog($className) {
         return CDaemon::createRunner($className)->rotateLog();
     }
 
+    /**
+     * Dump the log for a daemon service.
+     *
+     * @param string $className
+     *
+     * @return mixed
+     */
     public function logDump($className) {
         return CDaemon::createRunner($className)->logDump();
     }
 
+    /**
+     * Get the name of a daemon service.
+     *
+     * @param string $className
+     *
+     * @return string
+     */
     public function getServiceName($className) {
         $serviceName = $className;
         $serviceNameExploded = explode('_', $className);
-        if ($serviceNameExploded > 0) {
+        if (count($serviceNameExploded) > 0) {
             $serviceName = carr::get($serviceNameExploded, count($serviceNameExploded) - 1);
         }
 
         return $serviceName;
     }
 
+    /**
+     * Get the log file for a daemon service.
+     *
+     * @param string $className
+     * @param null|string $filename
+     *
+     * @return string
+     */
     public function getLogFile($className, $filename = null) {
         return CDaemon_Helper::getLogFile($className, $filename);
     }
 
+    /**
+     * Get the pid file for a daemon service.
+     *
+     * @param string $className
+     *
+     * @return string
+     */
     public function getPidFile($className) {
         return CDaemon_Helper::getPidFile($className);
     }
 
+    /**
+     * Get the list of log files for a daemon service.
+     *
+     * @param string $className
+     *
+     * @return array
+     */
     public function getLogFileList($className) {
         return CDaemon_Helper::getLogFileList($className);
     }
